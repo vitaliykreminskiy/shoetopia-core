00a82e076423e89fce6c43bb6402ac5c339662d0
Vitalii Kreminskyi <vkrmk13@gmail.com>
2026-05-22 12:41:22 +0300

    feat(admin/jobs): add shared types

---
+++ apps/admin/app/admin/jobs/types.ts
@@ -0,0 +1,49 @@
+export interface ActiveJob {
+  id: string
+  name: string
+  data: Record<string, any>
+  progress: number
+  processedOn: number | null
+}
+
+export interface WaitingJob {
+  id: string
+  name: string
+  data: Record<string, any>
+}
+
+export interface CompletedJob {
+  id: string
+  name: string
+  data: Record<string, any>
+  finishedOn: number | null
+  returnvalue: any
+}
+
+export interface FailedJob {
+  id: string
+  name: string
+  data: Record<string, any>
+  failedReason: string
+  attemptsMade: number
+}
+
+export interface QueueData {
+  name: string
+  active: ActiveJob[]
+  waitingCount: number
+  waiting: WaitingJob[]
+  completed: CompletedJob[]
+  failed: FailedJob[]
+}
+
+export interface JobsData {
+  queues: QueueData[]
+}
+
+export interface JobsStats {
+  active: number
+  waiting: number
+  failed: number
+  completedToday: number
+}
